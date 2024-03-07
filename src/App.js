fd405154988ed9ef7bb0c15ecb70d440943ab6e5
ayan <mohdayanrehman07@gmail.com>
2024-03-07 13:31:33 +0530

    routing{login/signup} done!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
