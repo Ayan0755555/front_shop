56ed32bbadeed627af86bff3820c2b5e96813d9d
ayan <mohdayanrehman07@gmail.com>
2024-03-09 12:34:14 +0530

    productdetails-checkout page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import CartPage from "./pages/CartPage";
+import Checkout from "./pages/CheckOut";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
+import ProductDetails from "./pages/ProductDetail";
 import SignupPage from "./pages/SignupPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -13,6 +15,8 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/productdetail" element={<ProductDetails />} />
         </Routes>
       </BrowserRouter>
     </div>
